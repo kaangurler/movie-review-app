340936f7ddf919b89e1accdf83ff955db9757028
Kaan Gurler <kaan.gurler@outlook.com>
2025-03-31 19:33:23 +0300

    Create rating entity

---
+++ backend/movie-service/src/main/java/io/github/kaangurler/movie_service/entity/Rating.java
@@ -0,0 +1,35 @@
+package io.github.kaangurler.movie_service.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "ratings")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Rating {
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+	private Long userId;
+	private Integer value;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "movie_id", referencedColumnName = "id")
+	@JsonIgnore
+	private Movie movie;
+}
