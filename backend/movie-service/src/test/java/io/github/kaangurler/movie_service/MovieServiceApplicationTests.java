74ec28bf8ea6def090e013407ed7dc209e2d4e76
Kaan Gurler <kaan.gurler@outlook.com>
2025-03-31 03:29:54 +0300

    Create project

---
+++ backend/movie-service/src/test/java/io/github/kaangurler/movie_service/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kaangurler.movie_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
