b34c6d9a745ba181d1b00393878e4a9c6fd009e4
Kaan Gurler <kaan.gurler@outlook.com>
2025-04-01 20:44:34 +0300

    Created movie controller

---
+++ backend/movie-service/src/main/java/io/github/kaangurler/movie_service/controller/MovieController.java
@@ -0,0 +1,11 @@
+package io.github.kaangurler.movie_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/movie/")
+@RequiredArgsConstructor
+public class MovieController {
+}
