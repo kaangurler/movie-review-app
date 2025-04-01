f78f4280fb4df76c3f7cdc5e593cc1e1f3529ae4
Kaan Gurler <kaan.gurler@outlook.com>
2025-04-01 21:00:21 +0300

    Created movie and rating services

---
+++ backend/movie-service/src/main/java/io/github/kaangurler/movie_service/service/MovieService.java
@@ -0,0 +1,11 @@
+package io.github.kaangurler.movie_service.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class MovieService {
+}
