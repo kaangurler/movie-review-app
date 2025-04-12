67d25f71345090b8c8aa3b213330922f6f02faa2
Kaan Gurler <kaan.gurler@outlook.com>
2025-04-13 01:18:52 +0300

    [Review-API] Implemented ReviewServiceImpl

--- backend/review-service/src/main/java/io/github/kaangurler/review_service/service/impl/ReviewServiceImpl.java
+++ backend/review-service/src/main/java/io/github/kaangurler/review_service/service/impl/ReviewServiceImpl.java
@@ -2,12 +2,16 @@ package io.github.kaangurler.review_service.service.impl;
 
 import io.github.kaangurler.review_service.dto.ReviewRequest;
 import io.github.kaangurler.review_service.dto.ReviewResponse;
+import io.github.kaangurler.review_service.entity.Review;
+import io.github.kaangurler.review_service.exception.custom.ReviewNotFoundException;
+import io.github.kaangurler.review_service.mapper.ReviewMapper;
 import io.github.kaangurler.review_service.repository.ReviewRepository;
 import io.github.kaangurler.review_service.service.ReviewService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,31 +24,54 @@ public class ReviewServiceImpl implements ReviewService {
 
 	@Override
 	public ReviewResponse create(ReviewRequest reviewRequest) {
-		return null;
+
+		Review review = ReviewMapper.toReview(reviewRequest);
+
+		Review dBReview = reviewRepository.save(review);
+
+		return ReviewMapper.toResponse(dBReview);
 	}
 
 	@Override
 	public ReviewResponse getById(UUID id) {
-		return null;
+
+		Review review = reviewRepository.findById(id)
+				.orElseThrow(() -> new ReviewNotFoundException("Review ID = " + id + " has not found"));
+
+		return ReviewMapper.toResponse(review);
 	}
 
 	@Override
 	public List<ReviewResponse> getAllByMovieId(UUID movieId) {
-		return List.of();
+
+		List<Review> reviews = reviewRepository.findByMovieId(movieId);
+
+		return reviews.parallelStream().map(ReviewMapper::toResponse).toList();
 	}
 
 	@Override
 	public List<ReviewResponse> getAllByUserId(UUID userId) {
-		return List.of();
+
+		List<Review> reviews = reviewRepository.findByUserId(userId);
+
+		return reviews.parallelStream().map(ReviewMapper::toResponse).toList();
 	}
 
 	@Override
 	public ReviewResponse update(UUID id, ReviewRequest reviewRequest) {
-		return null;
+
+		Review updatedReview = reviewRepository.findById(id).map(review -> {
+			review.setComment(reviewRequest.getComment());
+			review.setSpoiler(reviewRequest.getSpoiler());
+			review.setUpdated(LocalDateTime.now());
+			return review;
+		}).orElseThrow(() -> new ReviewNotFoundException("Review ID = " + id + " has not found"));
+
+		return ReviewMapper.toResponse(updatedReview);
 	}
 
 	@Override
 	public void delete(UUID id) {
-
+		// TODO : Create status enum and set it to deleted
 	}
 }
