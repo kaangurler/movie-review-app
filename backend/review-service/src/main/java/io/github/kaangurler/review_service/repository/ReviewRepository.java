3b31c2b1c00793779aac648a4c99def1d1b3823d
Kaan Gurler <kaan.gurler@outlook.com>
2025-04-13 01:18:08 +0300

    [Review-API] Added new methods to repository

--- backend/review-service/src/main/java/io/github/kaangurler/review_service/repository/ReviewRepository.java
+++ backend/review-service/src/main/java/io/github/kaangurler/review_service/repository/ReviewRepository.java
@@ -3,7 +3,11 @@ package io.github.kaangurler.review_service.repository;
 import io.github.kaangurler.review_service.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
+
+	List<Review> findByUserId(UUID userId);
+	List<Review> findByMovieId(UUID movieId);
 }
